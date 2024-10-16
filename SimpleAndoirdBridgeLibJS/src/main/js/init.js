05283ea73b47dab8c001de2d842f12ada08c1be2
hitman249 <hitman2491@gmail.com>
2024-10-16 18:27:30 +0700

    rewrite init.js to es5 syntax

--- SimpleAndoirdBridgeLibJS/src/main/js/init.js
+++ SimpleAndoirdBridgeLibJS/src/main/js/init.js
@@ -1,141 +1,162 @@
-const initBridge = (bridge, interfaces) => {
+var initBridge = function (bridge, interfaces) {
+  // check preconditions
+  if (!bridge) return;
+  if (!interfaces) return;
+  if (bridge.initialized) return;
 
-    // check preconditions
-    if (!bridge) return
-    if (!interfaces) return
-    if (bridge.initialized) return
+  function NativeError(message, nativeMessage, nativeStackTrace) {
+    Object.defineProperty(this, 'name', {
+      enumerable: false,
+      writable: false,
+      value: 'NativeError'
+    });
 
-    class NativeError extends Error {
+    this.message = message;
+    this.nativeMessage = nativeMessage;
+    this.nativeStackTrace = nativeStackTrace;
+    this.native = true;
 
-        constructor(message, nativeMessage, nativeStackTrace) {
-            super(message)
-            this.nativeMessage = nativeMessage
-            this.nativeStackTrace = nativeStackTrace
-            this.native = true
+    if (Error.hasOwnProperty('captureStackTrace')) { // V8
+      Error.captureStackTrace(this, NativeError);
+    } else {
+      Object.defineProperty(this, 'stack', {
+        enumerable: false,
+        writable: false,
+        value: (new Error(message)).stack
+      });
+    }
+
+    this.toString = function () {
+      if (!this.stack) {
+        return this.message + "\n" + "Caused by: " + this.nativeStackTrace;
+      } else {
+        return this.stack + "\n" + "Caused by: " + this.nativeStackTrace;
+      }
+    }.bind(this);
+  }
+
+  if (typeof Object.setPrototypeOf === 'function') {
+    Object.setPrototypeOf(NativeError.prototype, Error.prototype);
+  } else {
+    NativeError.prototype = Object.create(Error.prototype, {
+      constructor: {value: NativeError}
+    });
+  }
+
+
+  // enrich bridge
+  var functionBindings = [];
+  var currentFunctionBinding = 0;
+
+  var addFunctionBinding = function (f) {
+    functionBindings[currentFunctionBinding] = f;
+    return currentFunctionBinding++;
+  };
+
+  bridge.getFunction = function (functionBinding) {
+    return functionBindings[functionBinding];
+  };
+
+  bridge.removeFunction = function (functionBinding) {
+    delete functionBindings[functionBinding];
+  };
+
+  bridge.getFunctionBinding = function () {
+    return Object.keys(functionBindings);
+  };
+
+  bridge.executeFunction = function (functionBinding, arg) {
+    var f = bridge.getFunction(functionBinding);
+    setTimeout(function () {
+      f(arg);
+    });
+  };
+
+  bridge.executeFunctionWithPromiseBinding = function (functionBinding, promiseBinding, arg) {
+    var f = bridge.getFunction(functionBinding);
+    setTimeout(function () {
+      var promise = f(arg);
+      promise.then(function (ret) {
+        var answer = {hasError: false, isVoid: typeof ret === "undefined", value: ret};
+        bridge.finishPromise(promiseBinding, JSON.stringify(answer));
+      }).catch(function (err) {
+        var answer = {hasError: true, error: {message: err.toString(), stackTrace: err.stack}};
+        bridge.finishPromise(promiseBinding, JSON.stringify(answer));
+      });
+    });
+  };
+
+  bridge.interfaces = interfaces;
+
+  var prepareCall = function (call) {
+    call.arguments = call.arguments.map(function (arg) {
+      if (typeof arg === "function") {
+        return addFunctionBinding(arg);
+      } else {
+        return arg;
+      }
+    });
+  };
+
+  var callNativeInterface = function (call) {
+    prepareCall(call);
+    var callAsString = JSON.stringify(call);
+    var answerAsString = bridge.nativeCall(callAsString);
+    return JSON.parse(answerAsString);
+  };
+
+  bridge.nativeCallFullSync = function (call) {
+    var answer = callNativeInterface(call);
+    if (answer.hasError) {
+      throw new NativeError("Error in Native Layer: ", answer.error.message, answer.error.stackTrace);
+    }
+    if (!answer.isVoid) {
+      return answer.value;
+    }
+  };
+
+  bridge.nativeCallWebPromise = function (call) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        try {
+          resolve(bridge.nativeCallFullSync(call));
+        } catch (e) {
+          reject(e);
         }
+      });
+    });
+  };
 
-        toString() {
-            if (!this.stack) {
-                return this.message + "\n"+ "Caused by: " + this.nativeStackTrace
-            } else {
-                return this.stack + "\n"+ "Caused by: " + this.nativeStackTrace
-            }
-        }
-    }
-
-    // enrich bridge
-    const functionBindings = []
-    let currentFunctionBinding = 0
-
-    const addFunctionBinding = (f) => {
-        functionBindings[currentFunctionBinding] = f
-        return currentFunctionBinding++
-    }
-
-    bridge.getFunction = (functionBinding) => {
-        return functionBindings[functionBinding]
-    }
-
-    bridge.removeFunction = (functionBinding) => {
-        delete functionBindings[functionBinding]
-    }
-
-    bridge.getFunctionBinding = () => {
-        return Object.keys(functionBindings)
-    }
-    
-    bridge.executeFunction = (functionBinding, arg) => {
-        const f = bridge.getFunction(functionBinding)
-        setTimeout(() => {
-            f(arg)
-        })
-    }
-    
-    bridge.executeFunctionWithPromiseBinding = (functionBinding, promiseBinding, arg) => {
-        const f = bridge.getFunction(functionBinding)
-        setTimeout(() => {
-            const promise = f(arg)
-            promise.then((ret) => {
-                const answer = { hasError: false, isVoid: typeof ret === "undefined", value: ret }
-                bridge.finishPromise(promiseBinding, JSON.stringify(answer))
-            }).catch((err) => {
-                const answer = { hasError: true, error: { message: err.toString(), stackTrace: err.stack } }
-                bridge.finishPromise(promiseBinding, JSON.stringify(answer))
-            })
-        })
-    }
-
-    bridge.interfaces = interfaces
-
-    const prepareCall = (call) => {
-        call.arguments = call.arguments.map((arg) => {
-            if (typeof arg === "function") {
-                return addFunctionBinding(arg)
-            } else {
-                return arg
-            }
-        })
-    }
-
-    const callNativeInterface = (call) => {
-        prepareCall(call)
-        const callAsString = JSON.stringify(call)
-        const answerAsString = bridge.nativeCall(callAsString)
-        return JSON.parse(answerAsString)
-    }
-
-    bridge.nativeCallFullSync = (call) => {
-        const answer = callNativeInterface(call)
+  bridge.nativeCallFullPromise = function (call) {
+    return new Promise(function (resolve, reject) {
+      var functionBinding = addFunctionBinding(function (answer) {
         if (answer.hasError) {
-            throw new NativeError("Error in Native Layer: ", answer.error.message, answer.error.stackTrace)
+          reject(new NativeError("Error in Native Layer: ", answer.error.message, answer.error.stackTrace));
+        } else if (answer.isVoid) {
+          resolve();
+        } else {
+          resolve(answer.value);
         }
-        if (!answer.isVoid) {
-            return answer.value
+      });
+      setTimeout(function () {
+        try {
+          call.promiseFunctionBinding = functionBinding;
+          bridge.nativeCallFullSync(call);
+        } catch (e) {
+          bridge.removeFunction(functionBinding);
+          reject(e);
         }
-    }
+      });
+    });
+  };
 
-    bridge.nativeCallWebPromise = (call) => {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                try {
-                    resolve(bridge.nativeCallFullSync(call))
-                } catch (e) {
-                    reject(e)
-                }
-            })
-        })
-    }
+  // set flag to true
+  bridge.initialized = true;
 
-    bridge.nativeCallFullPromise = (call) => {
-        return new Promise((resolve, reject) => {
-            const functionBinding = addFunctionBinding((answer) => {
-                if (answer.hasError) {
-                    reject(new NativeError("Error in Native Layer: ", answer.error.message, answer.error.stackTrace))
-                } else if (answer.isVoid) {
-                    resolve()
-                } else {
-                    resolve(answer.value)
-                }
-            })
-            setTimeout(() => {
-                try {
-                    call.promiseFunctionBinding = functionBinding
-                    bridge.nativeCallFullSync(call)
-                } catch (e) {
-                    bridge.removeFunction(functionBinding)
-                    reject(e)
-                }
-            })
-        })
-    }
+  // call listener
+  if (bridge.afterInitialize) {
+    bridge.afterInitialize();
+  }
 
-    // set flag to true
-    bridge.initialized = true
-
-    // call listener
-    if (bridge.afterInitialize) {
-        bridge.afterInitialize()
-    }
-
-    bridge.nativeAfterInitialize()
-}
+  bridge.nativeAfterInitialize();
+};
